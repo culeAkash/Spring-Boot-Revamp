ad5bf3780da41a36c1a6ceb620f02ccb35cffd2d
Akash Jaiswal <akashjais929@gmail.com>
2025-06-07 13:42:37 +0530

    Initial Commit

---
+++ src/main/java/spring/boot/revamp/SpringBootRevampApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.revamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRevampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRevampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/revamp/SpringBootRevampApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.revamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRevampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
